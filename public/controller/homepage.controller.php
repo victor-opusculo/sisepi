ddca47f517ea55d6b27530eafcd2e92e45e14f91
Victor Opusculo Oliveira Ventura de Almeida <99560606+victor-opusculo@users.noreply.github.com>
2024-11-27 14:51:40 -0300

    Update homepage.controller.php

--- public/controller/homepage.controller.php
+++ public/controller/homepage.controller.php
@@ -4,7 +4,7 @@ final class homepage extends BaseController
 {
 	public function pre_home()
 	{
-		$this->title = "SisEPI - Sistema de Informações da Escola do Parlamento de Itapevi";
+		$this->title = "SisEPI - Sistema de Informações EPI";
 		$this->subtitle = null;
 	}
 }
